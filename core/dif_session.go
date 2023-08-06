a8844e0b9a206fba5b890d0e20ce16174d5ea63d
Pengwin61 <pengwin08@gmail.com>
2023-08-06 14:44:57 +0300

    вынес в отдельный файл все утилитки

--- core/dif_session.go
+++ core/dif_session.go
@@ -72,35 +72,6 @@ func OldDiffSession(x2gosession map[string]*connectors.User,
 	return err
 }
 
-func convertTime(t string) time.Time {
-
-	layout := "2006-01-02T15:04:05"
-	timeSession, err := time.Parse(layout, t)
-	if err != nil {
-		log.Println(err)
-	}
-	return timeSession
-}
-
-func checkExpirationSession(t, state string,
-	durationSession time.Duration) (bool, time.Duration) {
-
-	var msk, _ = time.ParseDuration("3h")
-
-	stopTimeSession := convertTime(t)
-	delta := time.Since(stopTimeSession)
-	delta = delta.Truncate(time.Second)
-
-	delta += msk
-
-	if delta >= durationSession && state != "R" {
-
-		return true, delta
-	}
-
-	return false, delta
-}
-
 func checkHostMatches(hostname, depSvcName, domain string) (bool, string) {
 
 	hostname = strings.TrimRight(hostname, fmt.Sprint(".", domain))
@@ -112,40 +83,4 @@ func checkHostMatches(hostname, depSvcName, domain string) (bool, string) {
 		fmt.Println("not found:", depSvcName, hostname, "are not equal")
 	}
 	return false, hostname
-}
-
-func containsIpaUser(array map[string]*connectors.User, value string) bool {
-	for k := range array {
-		if k == value {
-			return true
-		}
-	}
-	return false
-}
-func containsDbUser(array map[string]db.UserService, value string) bool {
-	for k := range array {
-		if k == value {
-			return true
-		}
-	}
-	return false
-}
-
-func difference(x2gosession map[string]*connectors.User, udssession map[string]db.UserService) (diff []string) {
-
-	diffArray := []string{}
-
-	for k := range x2gosession {
-		if !containsDbUser(udssession, k) {
-			diffArray = append(diffArray, k)
-		}
-	}
-
-	for k := range udssession {
-		if !containsIpaUser(x2gosession, k) {
-			diffArray = append(diffArray, k)
-		}
-	}
-
-	return diffArray
 }
--- core/show_session.go
+++ core/show_session.go
@@ -1,9 +1,5 @@
 package core
 
-import (
-	"fmt"
-)
-
 type ViewSession struct {
 	Username     string
 	Status       string
@@ -13,32 +9,6 @@ type ViewSession struct {
 }
 
 var Tmp = make([]ViewSession, 0)
-
-// func ShowSession(x2gosession map[string]*connectors.User) {
-// 	Tmp = nil
-
-// 	for k, v := range x2gosession {
-
-// 		v.StartDateSession = viewTimeFormat(v.StartDateSession)
-// 		v.StopDateSession = viewTimeFormat(v.StopDateSession)
-
-// 		switch v.SessionState {
-// 		case "S":
-// 			v.SessionState += "toped"
-
-// 		case "R":
-// 			v.SessionState += "unning"
-// 		}
-
-// 		vTmp := ViewSession{
-// 			Username:     k,
-// 			Status:       v.SessionState,
-// 			Hostname:     v.Hostname,
-// 			StartSession: v.StartDateSession,
-// 			StopSession:  v.StopDateSession}
-// 		Tmp = append(Tmp, vTmp)
-// 	}
-// }
 
 func ShowSession(personsSession *[]PersonSession) {
 	Tmp = nil
@@ -70,16 +40,4 @@ func ShowSession(personsSession *[]PersonSession) {
 		Tmp = nil
 	}
 
-}
-
-func viewTimeFormat(t string) string {
-
-	time := convertTime(t)
-
-	strDate := time.Format("02-01-2006")
-	strTime := time.Format("15:04:05")
-
-	res := fmt.Sprintln(strDate, "\n", strTime)
-
-	return res
 }
