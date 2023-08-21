7df35a824d59872439f44b49a2ec85aa42eb7f9e
Pengwin61 <pengwin08@gmail.com>
2023-08-21 16:42:02 +0300

    почистил

--- main.go
+++ main.go
@@ -39,31 +39,4 @@ func main() {
 	webClient.RunWeb(params.Web.User, params.Web.Pass,
 		params.Web.SslPub, params.Web.SslPriv)
 
-	// app := new(webapp.Application)
-	// app.Auth.Username = params.Web.User
-	// app.Auth.Password = params.Web.Pass
-
-	// mux := http.NewServeMux()
-	// mux.HandleFunc("/status", app.BasicAuth(app.ProtectedHandler))
-	// // mux.HandleFunc("/", app.UnprotectedHandler)
-
-	// fs := http.FileServer(http.Dir("templates"))
-	// mux.Handle("/", fs)
-
-	//
-	//
-	//
-	// srv := &http.Server{
-	// 	Addr:         fmt.Sprint("0.0.0.0:", params.Web.Port),
-	// 	Handler:      mux,
-	// 	IdleTimeout:  time.Minute,
-	// 	ReadTimeout:  10 * time.Second,
-	// 	WriteTimeout: 30 * time.Second,
-	// }
-	// log.Printf("starting server on %s", srv.Addr)
-
-	// err = srv.ListenAndServeTLS(params.SslPub, params.SslPriv)
-	// if err != nil {
-	// 	log.Printf("%s", err.Error())
-	// }
 }
